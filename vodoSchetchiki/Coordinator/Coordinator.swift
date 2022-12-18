1621133856a9a73e419a30ca2a1c6e71907a1893
Crocodil <evstarshov@icloud.com>
2022-12-18 15:49:45 +0300

    new improved Coordinator which includes LoginCoordinator + TabBarCoordinator
    (cherry picked from commit 5d67889d65890e3d4e1919ee518ba635ea2484ab)

--- vodoSchetchiki/Coordinator/Coordinator.swift
+++ vodoSchetchiki/Coordinator/Coordinator.swift
@@ -9,93 +9,60 @@ import Foundation
 import UIKit
 import FlagPhoneNumber
 
-class Coordinator {
-    
-    // MARK: - Properies
-    
-    internal var parentController: AppCoordinator?
-    internal var navigation: UINavigationController
-    
-    // MARK: - Init
-    
-    init(navigation: UINavigationController) {
-        self.navigation = navigation
-    }
-}
+/// Deprecated
 
-// MARK: - Extention
-
-extension Coordinator: AppCoordinator {
-    
-    //MARK: - Public functions
-    
-    public func start() {
-        goAuthController()
-    }
-    
-    public func goTabBar(fromVC: VerificationViewController) {
-        let tb = MainTabBarController()
-        navigation.pushViewController(tb, animated: true)
-    }
-    
-    public func ifUserDidAuth(fromVC: AuthViewController) {
-        let tb = MainTabBarController()
-        navigation.pushViewController(tb, animated: true)
-    }
-    
-    public func ifUserDeinit(from: SettingsViewController) {
-        let vc = AuthViewController()
-        let view = AuthView()
-        vc.coordinatorDelegate = self
-        vc.authView = view
-        navigation.pushViewController(vc, animated: true)
-    }
-    
-    //MARK: - Private functions
-    
-    private func goAuthController() {
-        let vc = AuthViewController()
-        let view = AuthView()
-        vc.coordinatorDelegate = self
-        vc.authView = view
-        navigation.pushViewController(vc, animated: true)
-    }
-    
-    private func goToVerification(fromVC: AuthViewController, verificationID: String) {
-        let vc = VerificationViewController()
-        let view = VerificationView()
-        vc.verificationView = view
-        vc.verificationView?.verificationID = verificationID
-        vc.modalPresentationStyle = .currentContext
-        vc.coordinatorDelegate = self
-        navigation.pushViewController(vc, animated: true)
-    }
-}
-
-
-extension Coordinator: VerificationViewDelegate {
-    
-    func goToMain(from: VerificationViewController) {
-        goTabBar(fromVC: from)
-    }
-}
-
-
-extension Coordinator: AuthViewControllerDelegate {
-    func showTabBarView(from: AuthViewController) {
-        ifUserDidAuth(fromVC: from)
-    }
-    
-    func showVerificationView(from: AuthViewController, verificationID: String) {
-        goToVerification(fromVC: from, verificationID: verificationID)
-    }
-}
-
-extension Coordinator: SettingsViewControllerDelegate {
-    func deinitUser(from: SettingsViewController) {
-        ifUserDeinit(from: from)
-    }
-    
-    
-}
+//class AppCoordinator {
+//    
+//    // MARK: - Properies
+//    
+//    internal var parentController: AppCoordinator?
+//    internal var navigation: UINavigationController
+//    
+//    // MARK: - Init
+//    
+//    init(navigation: UINavigationController) {
+//        self.navigation = navigation
+//    }
+//}
+//
+//// MARK: - Extention
+//
+//extension AppCoordinator: Coo {
+//    
+//    //MARK: - Public functions
+//    
+//    public func start() {
+//        goAuthController()
+//    }
+//    
+//    public func goTabBar(fromVC: VerificationViewController) {
+//        let tb = MainTabBarController()
+//        navigation.pushViewController(tb, animated: true)
+//    }
+//    
+//    public func ifUserDidAuth(fromVC: AuthViewController) {
+//        let tb = MainTabBarController()
+//        navigation.pushViewController(tb, animated: true)
+//    }
+//    
+//    public func ifUserDeinit(from: SettingsViewController) {
+//        let vc = AuthViewController()
+//        let view = AuthView()
+//        vc.coordinatorDelegate = self
+//        vc.authView = view
+//        navigation.pushViewController(vc, animated: true)
+//    }
+//    
+//    //MARK: - Private functions
+//    
+//    private func goAuthController() {
+//        let vc = AuthViewController()
+//        let view = AuthView()
+//        vc.coordinatorDelegate = self
+//        vc.authView = view
+//        navigation.pushViewController(vc, animated: true)
+//    }
+//    
+//
+//}
 
--- vodoSchetchiki/Coordinator/CoordinatorProtocol.swift
+++ vodoSchetchiki/Coordinator/CoordinatorProtocol.swift
@@ -9,8 +9,28 @@ import Foundation
 import UIKit
 
 
-protocol AppCoordinator {
-    var parentController: AppCoordinator? { get set }
-    var navigation: UINavigationController { get set }
+protocol Coordinator: AnyObject {
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+    var type: CoordinatorType { get }
     func start()
+    func finish()
+}
+
+extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
+}
+
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinator: Coordinator)
+}
+
+
+enum CoordinatorType {
+    case app, login, tab
 }
